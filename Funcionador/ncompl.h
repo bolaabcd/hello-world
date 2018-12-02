df49d44e420d381e56685b22f0328cc4247158c8
bolaabcd <35079449+bolaabcd@users.noreply.github.com>
2018-12-02 12:00:14 -0200

    Comentários

--- Funcionador/ncompl.h
+++ Funcionador/ncompl.h
@@ -1,28 +1,29 @@
 #ifndef NCOMPL_H
 #define NCOMPL_H
+//Números complexos!
 
 class ncompl
 {
     public:
-        ncompl();
-        rcncompl(double repo,double arim, bool polrec);
-        setc(double repo,double arim, bool polrec);
-        setc(double repo,double arim, char rp);
-        ncompl add(ncompl* outros);
-        ncompl mult(ncompl* outros);
-        ncompl elev(double outro);
-        double getInfo(bool repo,bool more_arim);
-        double getInfo(char rima);
-        operator+=(ncompl outro);
-        operator*=(ncompl outro);
-        operator^=(double real);
-        operator/=(ncompl outro);
-        operator=(ncompl outro);
-        fromDouble(double d);
+        ncompl();//Só declara
+        rcncompl(double repo,double arim, bool polrec);//Inicializa o garoto da forma ruim
+        setc(double repo,double arim, bool polrec);//Seta o número da forma ruim
+        setc(double repo,double arim, char rp);//Seta o número de forma boa
+        ncompl add(ncompl* outros);//Soma
+        ncompl mult(ncompl* outros);//Multiplica
+        ncompl elev(double outro);//Eleva
+        double getInfo(bool repo,bool more_arim);//Pega o número complexo de forma ruim
+        double getInfo(char rima);//Pega o número complexo de forma boa
+        operator+=(ncompl outro);//Soma
+        operator*=(ncompl outro);//Multiplica
+        operator^=(double real);//Eleva
+        operator/=(ncompl outro);//Divide
+        operator=(ncompl outro);//Seta
+        fromDouble(double d);//Seta a partir de real
 
     protected:
-        double mod;
-        double argu;
+        double mod;//Módulo
+        double argu;//Argumento
     private:
 
 };
